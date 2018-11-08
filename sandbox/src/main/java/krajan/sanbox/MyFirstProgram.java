6db04ae7f7f706fd1cffbca3b04ca6f4c5766b8d
AdKrajan <35641554+AdKrajan@users.noreply.github.com>
2018-11-08 15:02:01 +0100

    pierwsza zmienna

--- sandbox/src/main/java/krajan/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/krajan/sanbox/MyFirstProgram.java
@@ -5,9 +5,7 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println(2+2);
-        System.out.println(2-2);
         System.out.println(2*2);
-        System.out.println(2/2);
         System.out.println(1.0 / 2);
         System.out.println("Adam" + " Krajan"); //łączenie stringów konkatenacja
         System.out.println(2+2*2);
