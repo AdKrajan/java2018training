639a9c8597cce4a4e1275168ea521ecf445cde76
AdKrajan <35641554+AdKrajan@users.noreply.github.com>
2018-11-14 16:52:51 +0100

    L2F10 cholernie trudne

---
+++ addressbook-web-tests/src/test/java/pl/krajan/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package pl.krajan.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by adamk on 14.11.2018
+ **/
+public class NavigationHelper {
+    private FirefoxDriver wd;
+
+    public NavigationHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+}
